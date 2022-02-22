8f143bddf7a0b1687a535f8689ab21708fcb770d
Jared Remsberg <jaredremsberg@gmail.com>
2022-02-22 14:12:08 -0500

    set up music cog

---
+++ cogs/music.py
@@ -0,0 +1,25 @@
+import discord
+from common.cfg import devguilds
+from discord.commands import slash_command
+import yt_dlp
+
+
+class MusicPlayer(discord.Cog):
+    """Handles simple commands and listeners."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @slash_command(name="play", guild_ids=devguilds)
+    async def howdy(self, ctx, song: str):
+        """Command to start the music player."""
+        await ctx.respond(f"In progress!")
+
+
+def setup(bot):
+    """Entry point for loading cogs. Required for all cogs"""
+    bot.add_cog(MusicPlayer(bot))
+
+
+def fetch_song(query: str):
+    pass
