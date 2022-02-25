39107166322e066ede6180ba6237b4f40d3a2b24
dsmarcot2018 <61425027+dsmarcot2018@users.noreply.github.com>
2022-02-25 15:11:07 -0500

    Update database.py

--- database.py
+++ database.py
@@ -6,7 +6,13 @@ ID_Length = 16
 parent_path = "C:\\Users\\elizt\\PycharmProjects\\pythondb\\"
 
 
+# Database Object
+# Acts as a folder holding the tables within the database
 class HallBotDB(object):
+    # Initialize the database with just a name which will be the name of the folder.
+    # Creates the list of tables, named database.
+    # If the folder already exists it will load the database from memory.
+    # @param string name: The name of your database. Acts as the name of the folder.
     def __init__(self, name):
         self.database = []
         self.name = name
@@ -16,22 +22,31 @@ class HallBotDB(object):
         else:
             os.mkdir(self.path)
             self.ID = str(''.join(random.choices(string.ascii_letters + string.digits, k=ID_Length)))  # https://www.javatpoint.com/python-program-to-generate-a-random-string
-
+    
+    # Adds a table to your database.
+    # Your table will automatically have "ID" as the first header.
+    # @param string name: The name of your table. Will act as the file name for your table.
+    # @param list headers: A list of the headers for each column in your table.
     def create_table(self, name, headers):
         self.database.append(Table(name, headers))
-
+    
+    # Deletes the named table from your database.
+    # @param string name: The name of the table you would like to delete from the database.
     def delete_table(self, name):
         for each in self.database:
             if each.name == name:
                 self.database.remove(each)
-
+    
+    # Fetches a table object from your database.
+    # @param string name: The name of the table you would like to fetch from the database.
     def get_table(self, name):
         for each in self.database:
             if each.name == name:
                 return each
         return False
-
-    def loaddb(self):
+    
+    # Loads the database from memory.
+    def __loaddb(self):
         for file in os.listdir(self.path):
             table_name = file.split(".")[0]
             file_path = os.path.join(self.path, file)
@@ -42,7 +57,9 @@ class HallBotDB(object):
             for line in lines:
                 print(line[:-1].split(":"))
                 self.get_table(table_name).add_attribute(Attribute(line[:-1].split(":"), True))
-
+    
+    # Saves the database to memory.
+    # Will overwrite any files or folders with the same names.
     def dumpdb(self):
         for table in self.database:
             file_path = os.path.join(self.path, table.name + ".txt")
@@ -59,30 +76,53 @@ class HallBotDB(object):
                 file.close()
 
 
+# Table Object
+# Acts as a .txt file holding rows of attributes
 class Table(object):
+    # Initializes a table to put in the database.
+    # Creates a list of attributes and adds the headers as the first attribute.
+    # Your table will automatically have "ID" as the first header.
+    # @param string name: The name of the table. Acts as the name of the .txt file when saved to memory.
+    # @param list headers: A list of the headers for each column in your table.
     def __init__(self, name, headers):
         self.name = name
         self.attributes = []
         header_attribute = Attribute(headers, True)
         self.attributes.append(header_attribute)
         self.ID = str(''.join(random.choices(string.ascii_letters + string.digits, k=ID_Length)))
-
+    
+    # Adds an attribute to the attributes list.
+    # Effectively adds a row to your table.
+    # Attribute must have same number of values as the header for your table.
+    # @param Attribute attribute: The attribute that you are adding to the table.
     def add_attribute(self, attribute):
         if len(self.attributes[0].values) == len(attribute.values):
             self.attributes.append(attribute)
             return True
         else:
             return False
-
+    
+    # Removes an attribute from the attributes list.
+    # Effectively removes a row from your table.
+    # @param string ID: The ID of the attribute you would like to remove.
     def remove_attribute(self, ID):
         for each in self.attributes:
             if each.ID == ID:
                 self.attributes.remove(each)
 
-
+# Attribute Object
+# Acts as a row in your table .txt file.
 class Attribute(object):
+    # Initializes an attribute to put in your table.
+    # Copies a list of values and stores that in the attribute.
+    # Automatically adds a unique ID as the first value in attribute.
+    # If this is the header attribute it will add "ID" as the first value.
+    # @param list values: A list of values that is being added to the attribute
+    # @param bool header: Tells the program whether it is a header or not.
     def __init__(self, values, header):
         self.values = values
         self.ID = str(''.join(random.choices(string.ascii_letters + string.digits, k=ID_Length)))
-        if not header:
+        if header:
+            self.values.isert(0, "ID")
+        else:
             self.values.insert(0, self.ID)
