aef25f627bd34ce2a560995f2e8cf866bf40fd1e
Ryan LaVigne <61425102+RyanLavigne421@users.noreply.github.com>
2022-03-01 16:47:59 -0500

    Little Update

--- cogs/rps/rockpaperscissors.py
+++ cogs/rps/rockpaperscissors.py
@@ -18,8 +18,17 @@ class RockButton(discord.ui.Button):
         # self.choice = RPS.ROCK
         # return self.choice
         # Checks if player is already in rock
-        # if interaction.user in self.choice.rock:
+        if interaction.user in self.playerchoice.rock:
+            return
+
+        if interaction.user in self.playerchoice.paper:
+            self.playerchoice.paper.remove(interaction.user)
+
+        if interaction.user in self.playerchoice.scissors:
+            self.playerchoice.scissors.remove(interaction.user)
+
         self.playerchoice.rock.add(interaction.user)
+        # print(f"Rock: {self.playerchoice.rock}, Paper: {self.playerchoice.paper}, Scissors {self.playerchoice.scissors}")
         await interaction.response.send_message("You Picked Rock")
         # await interaction.response.edit_message(embed=self.RPSClass.embed)
 
@@ -32,7 +41,18 @@ class PaperButton(discord.ui.Button):
         self.playerchoice = playerchoice
 
     async def callback(self, interaction: discord.Interaction):
+
+        if interaction.user in self.playerchoice.paper:
+            return
+
+        if interaction.user in self.playerchoice.rock:
+            self.playerchoice.rock.remove(interaction.user)
+
+        if interaction.user in self.playerchoice.scissors:
+            self.playerchoice.scissors.remove(interaction.user)
+
         self.playerchoice.paper.add(interaction.user)
+        # print(f"Rock: {self.playerchoice.rock}, Paper: {self.playerchoice.paper}, Scissors {self.playerchoice.scissors}")
         await interaction.response.send_message("You Picked Paper")
 
 class ScissorButton(discord.ui.Button):
@@ -42,60 +62,69 @@ class ScissorButton(discord.ui.Button):
         self.playerchoice = playerchoice
 
     async def callback(self, interaction: discord.Interaction):
-        self.playerchoice.scissors.add(interaction.user)
-        await interaction.response.send_message("You Picked Scissors")
+        # if interaction.user in self.playerchoice.scissors:
+        #     return
 
-# class RPS:
-#     ROCK = "rock"
-#     PAPER = "paper"
-#     SCISSORS = "scissors"
+        # if interaction.user in self.playerchoice.rock:
+        #     self.playerchoice.rock.remove(interaction.user)
 
-#     def get_choices(self):
-#         return(self.ROCK, self.PAPER, self.SCISSORS)
+        # if interaction.user in self.playerchoice.paper:
+        #     self.playerchoice.paper.remove(interaction.user)
+
+        if interaction.user not in self.playerchoice.scissors:
+            self.playerchoice.scissors.add(interaction.user)
+            print(f"Rock: {self.playerchoice.rock}, Paper: {self.playerchoice.paper}, Scissors {self.playerchoice.scissors}")
+            # RPSGame.run(self, "scissors")
+            user_choice = self.playerchoice.scissors
+            winner, bot_choice = RPSGame.run("scissors")
+
+            if winner is None:
+                message = "It's a draw! Both chose: %s" % user_choice
+            elif winner is True:
+                message = "You win: %s vs %s" % (user_choice, bot_choice)
+            elif winner is False:
+                message = "You lose: %s vs %s" % (user_choice, bot_choice)
+        # print(f"Rock: {self.playerchoice.rock}, Paper: {self.playerchoice.paper}, Scissors {self.playerchoice.scissors}")
+            # await interaction.response.send_message("You Picked Scissors")
+            await interaction.response.send_message(message)
+
+
+class RPS:
+    ROCK = "rock"
+    PAPER = "paper"
+    SCISSORS = "scissors"
+
+    def get_choices(self):
+        return(self.ROCK, self.PAPER, self.SCISSORS)
     
-#     def check_win(self, choice1, choice2):
-#         winner_check = {
-#             (RPS.ROCK, RPS.PAPER): False,
-#             (RPS.ROCK, RPS.SCISSORS): True,
-#             (RPS.PAPER, RPS.ROCK): True,
-#             (RPS.PAPER, RPS.SCISSORS): False,
-#             (RPS.SCISSORS, RPS.ROCK): False,
-#             (RPS.SCISSORS, RPS.PAPER): True,
-#         }
+    def check_win(self, choice1, choice2):
+        winner_check = {
+            (RPS.ROCK, RPS.PAPER): False,
+            (RPS.ROCK, RPS.SCISSORS): True,
+            (RPS.PAPER, RPS.ROCK): True,
+            (RPS.PAPER, RPS.SCISSORS): False,
+            (RPS.SCISSORS, RPS.ROCK): False,
+            (RPS.SCISSORS, RPS.PAPER): True,
+        }
 
-#         winner = None
-#         if choice1 == choice2:
-#             winner = None
-#         else:
-#             winner = winner_check[(choice1, choice2)]
+        winner = None
+        if choice1 == choice2:
+            winner = None
+        else:
+            winner = winner_check[(choice1, choice2)]
 
-#         return winner      
+        return winner      
 
-# class RPSGame:
-#     def run(self, user_choice):
-#         rps_instance = RPS()
+class RPSGame():
+    def run(self, user_choice):
+        rps_instance = RPS()
 
-#         if user_choice not in rps_instance.get_choices():
-#             raise Exception("Invalid Choice: %s is not a valid choice" % user_choice)
+        bot_choice = rand.choice(rps_instance.get_choices())
 
-#         bot_choice = rand.choice(rps_instance.get_choices())
+        winner = rps_instance.check_win(user_choice, bot_choice)
 
-#         winner = rps_instance.check_win(user_choice, bot_choice)
+        return winner, bot_choice
 
-#         return winner, bot_choice
-
-# class Parser:
-#     def __init__(self, choice):
-#         choice = choice.lower()
-
-#         if choice == RPS.ROCK:
-#             self.choice = RPS.ROCK
-#         elif choice == RPS.PAPER:
-#             self.choice = RPS.PAPER
-#         elif choice == RPS.SCISSORS:
-#             self.choice = RPS.SCISSORS
-#         else:
-#             raise
 
 class RPSClass:
     def __init__(self):
@@ -114,8 +143,6 @@ class RPSClass:
     @property
     def controller(self):
         return discord.ui.View(RockButton(self), PaperButton(self), ScissorButton(self), timeout=60)
-
-
 
 class Rockpaperscissors(discord.Cog):
     """Play Rock Paper Scissors"""
@@ -148,6 +175,8 @@ class Rockpaperscissors(discord.Cog):
         choice = RPSClass()
         message = await ctx.respond(embed=choice.embed, view=choice.controller)
         self.playerchoice[message.id] = choice
+
+
 
 
 def setup(bot):
