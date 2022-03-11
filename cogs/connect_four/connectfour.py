943059a4b9b6519c3532c8d2638f43ca705d33b2
Jared Remsberg <jaredremsberg@gmail.com>
2022-03-11 12:11:14 -0500

    good enough for demo

--- cogs/connect_four/connectfour.py
+++ cogs/connect_four/connectfour.py
@@ -1,6 +1,7 @@
 import discord
 from common.cfg import devguilds
 from discord.commands import slash_command, Option
+from .game import Game
 
 
 class ConnectFour(discord.Cog):
@@ -8,6 +9,7 @@ class ConnectFour(discord.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.instances = {}
 
     @slash_command(name="connect4", guild_ids=devguilds)
     async def begin_connect4(
@@ -16,7 +18,11 @@ class ConnectFour(discord.Cog):
         opponent: Option(discord.Member, description="Player 2")
     ):
         """Begin a game of Connect Four with a friend."""
-        await ctx.respond(f"Howdy {ctx.author.mention}!")
+
+        game = Game(ctx.author, opponent)
+        self.instances[(ctx.author.id, opponent.id)] = game
+
+        await ctx.respond(embed=game.embed, view=game.view)
 
 
 def setup(bot):
